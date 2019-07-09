187cc6543a87f659dd70507689d6596be8f075b2
Kjell Magne Fauske <kjellmf@gmail.com>
2019-07-09 10:29:18 +0200

    Move specific options to store

--- orbatchart-ui/src/store/chart-module.ts
+++ orbatchart-ui/src/store/chart-module.ts
@@ -1,9 +1,19 @@
 import { GetterTree, Module, MutationTree } from "vuex";
 import { RootState } from "@/store/index";
-import { DEFAULT_OPTIONS, LevelLayout, OrbChartOptions } from "orbatchart";
+import {
+  DEFAULT_OPTIONS,
+  LevelLayout,
+  NumberMap,
+  OrbChartOptions,
+  PartialOrbChartOptions,
+  SpecificOptions, StringNumberMap
+} from "orbatchart";
 
 export interface ChartState {
   chartOptions: Partial<OrbChartOptions>;
+  levelOptions: NumberMap<PartialOrbChartOptions>;
+  levelGroupOptions: StringNumberMap<PartialOrbChartOptions>;
+  unitOptions: StringNumberMap<PartialOrbChartOptions>;
 }
 
 const state: ChartState = {
@@ -11,7 +21,19 @@ const state: ChartState = {
     ...DEFAULT_OPTIONS,
     maxLevels: 4,
     lastLevelLayout: LevelLayout.TreeRight
+  },
+  levelGroupOptions: {},
+  levelOptions: {},
+  unitOptions: {}
+  /*
+  levelOptions: { 1: { symbolSize: 50, debug: true, connectorOffset: 33 } },
+  levelGroupOptions: { "u0xGEGuOLzIcIirsioK0h": { symbolSize: 20, treeOffset: 120, connectorOffset: 20, debug: true } },
+  unitOptions: {
+    "u0xGEGuOLzIcIirsioK0h": { symbolSize: 40 },
+    "bJewqRWgkcxt3Lt53sz_c": { treeOffset: 60, symbolSize: 30, connectorOffset: 40 }
   }
+
+   */
 };
 
 const mutations: MutationTree<ChartState> = {
@@ -20,7 +42,11 @@ const mutations: MutationTree<ChartState> = {
   }
 };
 
-const getters: GetterTree<ChartState, RootState> = {};
+const getters: GetterTree<ChartState, RootState> = {
+  specificOptions: (state): SpecificOptions => {
+    return { level: state.levelOptions, levelGroup: state.levelGroupOptions, unit: state.unitOptions }
+  }
+};
 
 export default {
   state,
